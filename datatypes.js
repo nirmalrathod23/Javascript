7b9423088c61a281e56f83737abefef6f350a4e5
Nirmal Rathod <142677259+nirmalrathod23@users.noreply.github.com>
2023-10-13 09:53:28 +0000

    Datatypes of javascript

---
+++ datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // Treats all js files as latest version
+
+let name = "name"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value / object
+// undefined => undefined
+// symbol => unique
+
+
+// object
+
+console.log(typeof(undefined)); // undefined
+console.log(typeof null); // object
+console.log(typeof "4444"); // object
+console.log(12 + 156); // object
+let makrs = 156
+if(typeof(makrs) == "number"){
+    console.log(12 + makrs)
+}
