c6703d6b4e6b611dc5eeaaaba0c9041c62d77fe7
Nirmal Rathod <142677259+nirmalrathod23@users.noreply.github.com>
2023-10-13 09:57:59 +0000

    conversation of dataypes

---
+++ conversation.js
@@ -0,0 +1,36 @@
+let score = false;
+
+// console.log(typesof score); 
+// console.log(typesof(score)); use as a function
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);  // number
+// console.log(valueInNumber); //NaN
+
+// if(typeof(valueInNumber) == "number"){
+//     if(valueInNumber == "NaN"){
+//         console.log("not a Number")
+//     }
+//     else{
+//         console.log(12 + valueInNumber)
+//     }
+// }
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "name" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
